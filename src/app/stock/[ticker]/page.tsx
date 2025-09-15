133c48545ed58ad8458df36bc3af19e7dff74cb1
migotto7 <elthon.migotto7@gmail.com>
2025-09-15 12:04:03 -0300

    feat: new css style to ticker page

--- src/app/stock/[ticker]/page.tsx
+++ src/app/stock/[ticker]/page.tsx
@@ -1,7 +1,7 @@
 import NavbarTicker from "@/components/NavbarTicker";
 import StockBarChart from "@/components/StockBarChart";
 import { HistoricalDataPrice } from "@/type/HistoricalDataPrice";
-import { MoveUp, MoveDown, ChartColumnIncreasing, DollarSign, Percent, Landmark, HandCoins } from "lucide-react";
+import { MoveUp, MoveDown, ChartColumnIncreasing, DollarSign, Percent, Landmark, HandCoins, Triangle } from "lucide-react";
 import Image from "next/image";
 
 interface StockProps {
@@ -58,7 +58,7 @@ export default async function StockPage({ params }: StockProps) {
                 </div>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-8">
-                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4">
+                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4 gap-1">
                         <div className="bg-green-900 w-14 h-14 md:w-16 md:h-16 rounded-2xl flex items-center justify-center">
                             <DollarSign className="text-green-500" />
                         </div>
@@ -67,18 +67,22 @@ export default async function StockPage({ params }: StockProps) {
                             <h2 className="text-white text-lg md:text-xl font-semibold pt-1 md:pt-2">R$ {stock.regularMarketPrice.toFixed(2)}</h2>
                         </div>
                     </div>
-                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4">
+                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4 gap-1">
                         <div className="bg-emerald-900 w-14 h-14 md:w-16 md:h-16 rounded-2xl flex items-center justify-center">
                             <Percent className="text-emerald-500" />
                         </div>
                         <div className="mx-4">
                             <span className="text-sm md:text-md text-gray-300">Variação</span>
                             <h2 className={`text-lg md:text-xl font-semibold pt-1 md:pt-2 flex items-center ${stock.regularMarketChange >= 0 ? "text-green-500" : "text-red-500"}`}>
-                                {stock.regularMarketChangePercent.toFixed(2)}% {stock.regularMarketChange >= 0 ? <MoveUp /> : <MoveDown />}
+                                {stock.regularMarketChangePercent.toFixed(2)}%
+
                             </h2>
                         </div>
+                        <div className={`rounded-4xl p-1 flex items-center justify-center ${stock.regularMarketChange >= 0 ? "text-green-500 bg-green-900" : "text-red-500 bg-red-900"}`}>
+                            {stock.regularMarketChange >= 0 ? <MoveUp /> : <MoveDown />}
+                        </div>
                     </div>
-                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4">
+                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4 gap-1">
                         <div className="bg-teal-900 w-14 h-14 md:w-16 md:h-16 rounded-2xl flex items-center justify-center">
                             <Landmark className="text-teal-500" />
                         </div>
@@ -87,7 +91,7 @@ export default async function StockPage({ params }: StockProps) {
                             <h2 className="text-white text-lg md:text-xl font-semibold pt-1 md:pt-2">{stock.priceEarnings.toFixed(2)}</h2>
                         </div>
                     </div>
-                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4">
+                    <div tabIndex={0} className="bg-gray-800 w-full rounded-xl flex items-center px-3 py-4 gap-1">
                         <div className="bg-orange-900 w-14 h-14 md:w-16 md:h-16 rounded-2xl flex items-center justify-center">
                             <HandCoins className="text-orange-500" />
                         </div>
@@ -98,18 +102,29 @@ export default async function StockPage({ params }: StockProps) {
                     </div>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mb-8">
-                    <div tabIndex={0} className="bg-gray-800 w-full px-3 py-3 md:px-4 md:py-5 rounded-xl">
+                    <div tabIndex={0} className="bg-gray-800 w-full px-3 py-4 md:px-4 rounded-xl flex flex-col justify-center gap-y-3 md:gap-1">
                         <span className="text-sm md:text-lg text-gray-300">Faixa do dia</span>
                         <div className="my-2 md:my-4">
-                            <div className="w-full bg-gray-500 h-2 md:h-3 rounded overflow-hidden relative">
+                            <div className="w-full bg-gray-500 h-2 md:h-3 rounded overflow-visible relative">
                                 <div
-                                    className="absolute top-1/2 -translate-y-1/2 w-2 h-3 bg-green-500 shadow-lg"
+                                    className="absolute flex flex-col items-center z-10"
                                     style={{
                                         left: `${((stock.regularMarketPrice - stock.regularMarketDayLow) /
                                             (stock.regularMarketDayHigh - stock.regularMarketDayLow)) * 100}%`,
+                                        top: "-16px",
                                         transform: "translateX(-50%)",
                                     }}
-                                />
+                                >
+                                    <Triangle
+                                        className="w-3 h-4 rotate-180"
+                                        fill="white"
+                                        stroke="white"
+                                    />
+                                    <div
+                                        className="w-2 h-2 md:h-3 bg-white"
+
+                                    />
+                                </div>
                             </div>
                             <div className="flex justify-between text-xs md:text-sm mt-2 text-gray-300">
                                 <span>R$ {stock.regularMarketDayLow.toFixed(2)}</span>
@@ -117,18 +132,29 @@ export default async function StockPage({ params }: StockProps) {
                             </div>
                         </div>
                     </div>
-                    <div tabIndex={0} className="bg-gray-800 w-full px-3 py-3 md:px-4 md:py-5 rounded-xl">
+                    <div tabIndex={0} className="bg-gray-800 w-full px-3 py-4 md:px-4 rounded-xl flex flex-col justify-center gap-y-3 md:gap-1">
                         <span className="text-sm md:text-lg text-gray-300">Faixa 52 semanas</span>
                         <div className="my-2 md:my-4">
-                            <div className="w-full bg-gray-500 h-2 md:h-3 rounded overflow-hidden relative">
+                            <div className="w-full bg-gray-500 h-2 md:h-3 rounded overflow-visible relative">
                                 <div
-                                    className="absolute top-1/2 -translate-y-1/2 w-2 h-3 bg-green-500 shadow-lg"
+                                    className="absolute flex flex-col items-center z-10"
                                     style={{
                                         left: `${((stock.regularMarketPrice - stock.fiftyTwoWeekLow) /
                                             (stock.fiftyTwoWeekHigh - stock.fiftyTwoWeekLow)) * 100}%`,
+                                        top: "-16px",
                                         transform: "translateX(-50%)",
                                     }}
-                                />
+                                >
+                                    <Triangle
+                                        className="w-3 h-4 rotate-180"
+                                        fill="white"
+                                        stroke="white"
+                                    />
+                                    <div
+                                        className="w-2 h-2 md:h-3 bg-white"
+
+                                    />
+                                </div>
                             </div>
                             <div className="flex justify-between text-xs md:text-sm mt-2 text-gray-300">
                                 <span>R$ {stock.fiftyTwoWeekLow.toFixed(2)}</span>
@@ -136,17 +162,27 @@ export default async function StockPage({ params }: StockProps) {
                             </div>
                         </div>
                     </div>
-                    <div tabIndex={0} className="bg-gray-800 w-full px-3 py-3 md:px-4 md:py-5 rounded-xl">
+                    <div tabIndex={0} className="bg-gray-800 w-full px-3 py-4 md:px-4 rounded-xl flex flex-col justify-center gap-y-3 md:gap-1">
                         <span className="text-sm md:text-lg text-gray-300">Preço justo</span>
                         <div className="my-2 md:my-4">
-                            <div className="w-full bg-gray-500 h-2 md:h-3 rounded overflow-hidden relative">
+                            <div className="w-full h-2 md:h-3 rounded overflow-visible relative bg-gradient-to-r from-red-400 via-yellow-400 to-green-400">
                                 <div
-                                    className="absolute top-1/2 -translate-y-1/2 w-2 h-3 bg-green-500 shadow-lg"
+                                    className="absolute flex flex-col items-center z-10"
                                     style={{
-                                        left: `${fairPrice}`,
+                                        left: `${fairPrice}%`,
+                                        top: "-16px",
                                         transform: "translateX(-50%)",
                                     }}
-                                />
+                                >
+                                    <Triangle
+                                        className="w-3 h-4 rotate-180"
+                                        fill="white"
+                                        stroke="white"
+                                    />
+                                    <div
+                                        className="w-2 h-2 md:h-3 bg-white"
+                                    />
+                                </div>
                             </div>
                             <div className="flex justify-between text-xs md:text-sm mt-2 text-gray-300">
                                 <span>R$ {stock.fiftyTwoWeekLow.toFixed(2)}</span>
