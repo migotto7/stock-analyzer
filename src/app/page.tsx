d473204678760e973831addf7b2b66d1e4aa23ba
migotto7 <elthon.migotto7@gmail.com>
2025-09-22 15:02:07 -0300

    feat: adding tag Head to improve SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import BarTopStocks from "@/components/BarTopStocks";
 import SearchBox from "@/components/SearchBox";
 import HomeButton from "@/components/HomeButton";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
-      <HomeButton />
-      <SearchBox />
-      <BarTopStocks />
-    </main>
+    <>
+      <Head>
+        <title>Análise de ações da Ibovespa</title>
+        <meta name="description" content="Veja análises, indicadores e gráficos das ações da Bovespa em tempo real." />
+        <meta property="og:title" content="Análise de Ações da Bovespa" />
+        <meta property="og:description" content="Descubra tendências e indicadores das principais ações brasileiras." />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
+        <HomeButton />
+        <SearchBox />
+        <BarTopStocks />
+      </main>
+    </>
   );
 }
